926497468b8b57c86b9ed77f7420b85cad5888d1
Aridane Martín <aridanemartin@gmail.com>
2024-01-24 19:39:47 +0000

    Add project files.

---
+++ Lemoncode.Lab.Api/Entities/Participant.cs
@@ -0,0 +1,11 @@
+﻿namespace Lemoncode.Lab.Api.Entities
+{
+    public class Participant
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public ICollection<Event> Events { get; set; }
+    }
+}
---
+++ Lemoncode.Lab.Api/Program.cs
@@ -0,0 +1,27 @@
+
+using Microsoft.EntityFrameworkCore;
+using Lemoncode.Lab.Api.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+//app.MapControllers();
+
+app.Run();
